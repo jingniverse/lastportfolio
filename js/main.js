37f0b33c898362c7f7a48dc9276925de55808f01
Yeong Hwan Kim <kch8317@gmail.com>
2024-03-06 00:44:21 +0900

    edittt

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+
+$('.hahaha img').click(function(){
+    $('.left').addClass('boxani')
+    $('.right').addClass('boxani2')
+    $('.hahaha img').addClass('totop')
+    $('.hahaha').addClass('totop2')
+})
+
+
+})
